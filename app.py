582554e68efcfecf67dac65d270bfe172d124a68
ALLEN <aljay3334@gmail.com>
2021-12-27 23:24:48 +0000

    Added route for adding note

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
@@ -6,6 +7,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:password$@localhost/flask_
 db = SQLAlchemy(app)
 
 
+@dataclass
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200))
@@ -16,20 +18,22 @@ class Note(db.Model):
         self.content = content
 
 
-note = Note("My Flask Note", "There are lot of apps powered by flask")
-db.create_all()
-db.session.commit()
+@app.route("/add_note", methods=["POST"])
+def add_note():
+    title = "My programming note"
+    content = "Computer programming........"
+    note = Note(title, content)
+    db.session.add(note)
+    db.session.commit()
+    return jsonify({"message": "Note Added"})
 
 
 @app.route("/notes", methods=["GET"])
 def fetch_notes():
     notes = Note.query.all()
-    my_notes = []
-    for my_note in my_notes:
-        my_notes.append(my_note.toDict())
-
-    return jsonify(my_notes)
+    return jsonify(notes)
 
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
