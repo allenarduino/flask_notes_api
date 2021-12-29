f82b9960952bf0db41c627e7f6f50ca598da4c8c
ALLEN <aljay3334@gmail.com>
2021-12-29 23:09:52 +0000

    Added @dataclass and few changes in Note class to make it json seriable

--- models/note.py
+++ models/note.py
@@ -1,9 +1,15 @@
 from database import db
+import json
 from dataclasses import dataclass
 
 
 @dataclass
 class Note(db.Model):
+
+    id: int
+    title: str
+    content: str
+
     __tablename__ = "note"
 
     id = db.Column(db.Integer, primary_key=True)
@@ -13,3 +19,6 @@ class Note(db.Model):
     def __init__(self, title, content):
         self.title = title
         self.content = content
+
+    def serialize(self):
+        return json.dumps(self, default=lambda o: o.__dict__)
