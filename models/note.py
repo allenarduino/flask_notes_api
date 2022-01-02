6b4b7e0dda2f241baa1842ebe0202dcd35672e87
ALLEN <aljay3334@gmail.com>
2022-01-02 12:37:58 +0000

    Added time_created field column to Note class or Table

--- models/note.py
+++ models/note.py
@@ -1,5 +1,6 @@
 from database import db
 import json
+import datetime
 from dataclasses import dataclass
 
 
@@ -9,14 +10,18 @@ class Note(db.Model):
     id: int
     title: str
     content: str
+ 
 
     __tablename__ = "note"
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200))
     content = db.Column(db.Text)
-    time_created=db.Column(db.Date,index=True)
+    time_created= db.Column(db.DateTime,default=datetime.datetime.now)
+  
 
     def __init__(self, title, content):
         self.title = title
         self.content = content
+
+       
